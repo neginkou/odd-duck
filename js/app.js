daf831097eb2895d7c8ecd40e8d3bdbd64cc2026
neginkou <negin.kou@gmail.com>
2023-11-07 21:44:54 -0800

    Second Attempt

--- js/app.js
+++ js/app.js
@@ -1,91 +1,145 @@
 const DuckContainer = document.getElementById("Duck");
-const ReoprtContainer = document.getElementById("Report");
-
-const image1 = document.querySelector('#Duck img:first-child');
-const image2 = document.querySelector('#Duck img:nth-child(2)');
-const image3 = document.querySelector('#Duck img:nth-child(3)');
-
+const mychart = document.getElementById("Chart")
 const Button = document.getElementById("ShowResults");
 
+
+const image1 = document.querySelector('#Duck figure:first-child img');
+const caption1 = document.querySelector('#Duck figure:first-child figcaption');
+const image2 = document.querySelector('#Duck figure:nth-child(2) img');
+const caption2 = document.querySelector('#Duck figure:nth-child(2) figcaption');
+const image3 = document.querySelector('#Duck figure:nth-child(3) img');
+const caption3 = document.querySelector('#Duck figure:nth-child(3) figcaption');
+
+
 let state = {
-    numClicksSoFar: 0,
-    numClicksAllowed: 25,
-    allDuck: [],
-}
+  numClicksSoFar: 0,
+  numClicksAllowed: 25,
+  allDuck: [],
+  threeImages: [],
+
+};
 
 function Duck(name, image) {
-    this.name = name;
-    this.imageFile = image;
-    this.votes = 0;
-    this.views = 0;
-    state.allDuck.push(this);
-}
-console.log(Duck);
-
-function renderImages(){
-    function pickRandomImage(){
-        return Math.floor(Math.random() * state.allDuck.length );
-    }
-    let Duck1 = pickRandomImage();
-    let Duck2 = pickRandomImage();
-    let Duck3 = pickRandomImage();
-
-    while(Duck1 === Duck2 || Duck1 === Duck3 || Duck2 === Duck3) {
-        Duck2 = pickRandomImage();
-        Duck3 = pickRandomImage();
-    }
-    image1.src = state.allDuck[Duck1].imageFile;
-    image1.alt = state.allDuck[Duck1].name;
-
-    image2.src = state.allDuck[Duck2].imageFile;
-    image2.alt = state.allDuck[Duck2].name;
-
-    image3.src = state.allDuck[Duck3].imagefile;
-    image3.src = state.allDuck[Duck3].name;
-
-    state.allDuck[Duck1].views++;
-    state.allDuck[Duck2].views++;
-    state.allDuck[Duck3].views++;
-
-}
-function removeButton(){
-    Button.style.display = 'none';
+  this.name = name;
+  this.imageFile = image;
+  this.votes = 0;
+  this.views = 0;
+  state.allDuck.push(this);
 }
 
+function renderDucks() {
+  function pickRandomDuck() {
+    return Math.floor(Math.random() * state.allDuck.length);
+  }
+
+  let Duck1 = pickRandomDuck();
+  let Duck2 = pickRandomDuck();
+  let Duck3 = pickRandomDuck();
+  while (Duck1 === Duck2 || Duck1 === Duck3 || state.threeImages.includes(Duck1)) {
+    Duck1 = pickRandomDuck();
+  }
+
+  while (Duck2 === Duck3 || Duck2 === Duck1 || state.threeImages.includes(Duck2)) {
+    Duck2 = pickRandomDuck();
+  };
+
+  while (Duck3 === Duck1 || Duck3 === Duck2 || state.threeImages.includes(Duck3)) {
+    Duck3 = pickRandomDuck();
+  }
+  image1.src = state.allDuck[Duck1].imageFile;
+  image1.alt = Duck1.name;
+  caption1.textContent = state.allDuck[Duck1].name;
+
+  image2.src = state.allDuck[Duck2].imageFile;
+  image2.alt = Duck2.name;
+  caption2.textContent = state.allDuck[Duck2].name;
+
+  image3.src = state.allDuck[Duck3].imageFile;
+  image3.alt = Duck3.name;
+  caption3.textContent = state.allDuck[Duck3].name;
+
+  state.allDuck[Duck1].views++;
+  state.allDuck[Duck2].views++;
+  state.allDuck[Duck3].views++;
+}
 function renderResultButton() {
-    Button.style.display = "block";
-}
-function ShowResults(){
-    for(let i = 0; i < state.allDuck.length; i++){
-        let Duckresult = document.createElement('p');
-        Duckresult.textContent = (`${state.allDuck[i].name} votes: ${Number(state.allDuck[i].votes)} views: ${state.allDuck[i].views}`);
-        DuckContainer.appendChild(Duckresult);
-    }
-
+  Button.style.display = "block";
 }
 
-function handleClick(event){
-    let imageName = event.target.alt;
-    for ( let i = 0; i < state.allDuck.length; i++){
-        if(imageName === state.allDuck[i].name){
-            state.allDuck[i].votes++;
-            break;
-        }
+function renderResults() {
+  let DuckNames = [];
+  let DuckVotes = [];
+  let DuckViews = [];
+
+  for (let i = 0; i < state.allDuck.length; i++) {
+    DuckNames.push(state.allDuck[i].name);
+    DuckVotes.push(state.allDuck[i].votes);
+    DuckViews.push(state.allDuck[i].views);
+  }
+  console.log(DuckNames);
+  console.log(DuckVotes);
+  console.log(DuckViews);
+
+  const data = {
+    labels: DuckNames,
+    datasets: [
+      {
+        label: 'Votes',
+        data: DuckVotes,
+        borderWidth: 1,
+        backgroundColor: ['green']
+
+      },
+    {
+      label: 'Views',
+      data: DuckViews,
+      borderWidth: 1,
+      backgroundColor: ['rebeccapurple']
     }
-    if(state.numClicksSoFar >= state.numClicksAllowed){
-        DuckContainer.removeEventListener("click", clickEvent);
-        removeEventListener();
-        renderResultButton();
-    }
-    else {
-        state.numClicksSoFar++;
-        renderImages();
-    }
+    ]
 }
-function setupListener(){
-    DuckContainer.addEventListener("click", handleClick);
-    Button.addEventListener("click",ShowResults);
+const config = {
+  type: 'bar',
+  data: data,
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
 }
+const myChart = new Chart(renderResults, config);
+}
+
+
+
+function handleClick(event) {
+  let DuckName = event.target.alt;
+  for (let i = 0; i < state.allDuck.length; i++) {
+    if (DuckName === state.allDuck[i].name) {
+      state.allDuck[i].votes++;
+      break;
+    }
+  }
+  state.numClicksSoFar++;
+
+  if (state.numClicksSoFar >= state.numClicksAllowed) {
+    removeListener();
+    renderResultButton();
+  } else {
+    renderDucks();
+  }
+}
+
+function setupListener() {
+  DuckContainer.addEventListener("click", handleClick);
+  Button.addEventListener("click", renderResults);
+}
+function removeListener() {
+  DuckContainer.removeEventListener("click", handleClick);
+}
+
 
 new Duck("R2D2 Bag", "images/assets/bag.jpg");
 new Duck("Banana", "images/assets/banana.jpg");
@@ -93,8 +147,8 @@ new Duck("Bathroom", "images/assets/bathroom.jpg");
 new Duck("Boots", "images/assets/boots.jpg");
 new Duck("Breakfast", "images/assets/breakfast.jpg");
 new Duck("Bubblegum", "images/assets/bubblegum.jpg");
-new Duck("Chair","images/assets/chair.jpg");
-new Duck("Cthulhu", "images/assets/cthulhu.jpg") ;
+new Duck("Chair", "images/assets/chair.jpg");
+new Duck("Cthulhu", "images/assets/cthulhu.jpg");
 new Duck("Dog Duck", "images/assets/dog-duck.jpg");
 new Duck("Dragon", "images/assets/dragon.jpg");
 new Duck("Pen", "images/assets/pen.jpg");
@@ -107,7 +161,8 @@ new Duck("Unicorn", "images/assets/unicorn.jpg");
 new Duck("Water can", "images/assets/water-can.jpg");
 new Duck("Wine glass", "images/assets/wine-glass.jpg");
 
-renderImages();
+
+renderDucks();
 setupListener();
 
 
